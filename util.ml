48b9b7a083a1458b1d6d2f67f5a5d20603a7413f
BGuillemet <115193742+BGuillemet@users.noreply.github.com>
2023-05-08 19:13:22 +0200

    debug printing

--- util.ml
+++ util.ml
@@ -14,12 +14,18 @@ let rec remove el l = List.filter (( <> ) el) l
 (** Supprime toutes les occurences des éléments de els dans l *)
 let remove_all els l = List.fold_left (fun acc el -> remove el acc) l els
 
-let clause_string c = String.concat " ⊕  " (List.map string_of_int c)
+let clause_string c = match c with
+  | [] -> "⊥"
+  | [0] -> "⊤"
+  | _ -> String.concat " ⊕ " (List.map string_of_int c)
+
 let print_clause c = print_string (clause_string c)
   
-let form_string f = String.concat " /\\ " (List.map clause_string @@ Array.to_list f)
+let form_string f = 
+  if f = [||] then "⊤"
+  else String.concat " /\\ " (List.map clause_string @@ Array.to_list f)
 
 let print_form f = print_string (form_string f)
 
 let print_val rho =
-  Array.iteri (fun i x -> if i <> 0 then Printf.printf "%d: %d\n" i x) rho
+  Array.iteri (fun i x -> if i <> 0 then Printf.printf "%d: %d\n" i (abs x)) rho
